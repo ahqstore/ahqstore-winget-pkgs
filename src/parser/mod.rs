4a689eacdfb076b20d7c9f6c18726e7311df9bde
AHQ Miness <ahqsecret@gmail.com>
2024-10-12 22:14:32 +0530

    fix

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -216,7 +216,7 @@ fn app_parse(letter: &str, author: &str, map: &mut Map) {
           let mut winarm = None;
         
           installer.Installers.into_iter().for_each(|x| {
-            let installer = match x.InstallerType.as_ref() {
+            let installer = match x.InstallerType.unwrap_or_default() {
               "msi" => Some(InstallerFormat::WindowsInstallerMsi),
               "exe" => Some(InstallerFormat::WindowsInstallerExe),
               _ => None,
