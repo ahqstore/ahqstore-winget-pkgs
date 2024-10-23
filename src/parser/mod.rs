684e9dd291dcf4e5f919494364ceaec310eca7f9
AHQ Miness <ahqsecret@gmail.com>
2024-10-23 21:58:49 +0530

    fix string

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -83,7 +83,7 @@ impl Map {
     let _ = self.search.write(
       format!(
         "{{\"name\": {:?}, \"title\": {:?}, \"id\": {:?}}}",
-        app.appDisplayName, app.appShortcutName, app.appId
+        fixstr(app.appDisplayName), fixstr(app.appShortcutName), fixstr(app.appId)
       )
       .as_bytes(),
     );
@@ -110,6 +110,13 @@ impl Map {
 
     let _ = fs::write("./db/total", self.files.to_string());
   }
+}
+
+fn fixstr(st: &str) -> String {
+  st.replace(
+    "\\u",
+    " "
+  )
 }
 
 pub fn parser() {
